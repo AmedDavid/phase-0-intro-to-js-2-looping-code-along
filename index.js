d435b653f4aec81b87f01726497fabffa410123c
David Amedi <amukoahdavid@gmail.com>
2025-02-27 00:22:29 +0300

    add functions to write thank you cards and count down

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+const writeCards = (names, event) => {
+    let messages = []
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages
+}
+
+const countDown = (num) => {
+    while (num >= 0) {
+        console.log(num)
+        num--
+    }
+}
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday")
+countDown(
+    10
+)
+
